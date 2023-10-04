08d6132bebc71a6831888f1f95ba9037f1cad911
JoseAPR <92516246+Jose-Antonio96@users.noreply.github.com>
2023-10-04 12:10:09 +0200

    configuracion

---
+++ src/test/java/com/producto/productoapi/ProductoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.producto.productoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
